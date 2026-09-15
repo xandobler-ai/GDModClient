148b3ac3321334db59a9d6840f0e00aa51637d2a
xandobler-ai <xandobler@gmail.com>
2026-09-15 14:28:45 +1000

    Update ModLoader.cpp

--- src/core/ModLoader.cpp
+++ src/core/ModLoader.cpp
@@ -1,5 +1,7 @@
 #include "gdmc/ModLoader.hpp"
+#include "gdmc/Http.hpp"
 
+#include <filesystem>
 #include <iostream>
 
 namespace gdmc {
@@ -9,7 +11,29 @@ bool ModLoader::install(const ModInfo& mod)
     std::cout << "[GDMC] Installing: "
               << mod.name << "\n";
 
-    // Downloading the .geode will be added later.
+    if (mod.downloadURL.empty())
+    {
+        std::cerr << "[GDMC] Mod has no download URL.\n";
+        return false;
+    }
+
+    std::filesystem::create_directories("mods");
+
+    std::string filename =
+        "mods/" + mod.id + ".geode";
+
+    if (!Http::download(
+            mod.downloadURL,
+            filename))
+    {
+        std::cerr << "[GDMC] Failed to install "
+                  << mod.name
+                  << "\n";
+
+        return false;
+    }
+
+    std::cout << "[GDMC] Installed successfully!\n";
 
     return true;
 }
