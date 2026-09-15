80ea0360e5131d5df58bf6bebd90221b628eecd7
xandobler-ai <xandobler@gmail.com>
2026-09-15 13:30:27 +1000

    Create ModInfo.hpp

---
+++ include/gdmc/ModInfo.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+
+namespace gdmc {
+
+struct ModInfo {
+    std::string id;
+    std::string name;
+    std::string version;
+    std::string developer;
+    std::string description;
+    std::string downloadURL;
+};
+
+}
