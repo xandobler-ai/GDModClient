bc135eff08a7d6733eac6beaf6ad16a101435738
xandobler-ai <xandobler@gmail.com>
2026-09-15 14:27:40 +1000

    Update Http.hpp

--- include/gdmc/Http.hpp
+++ include/gdmc/Http.hpp
@@ -7,6 +7,11 @@ namespace gdmc {
 class Http {
 public:
     static std::string get(const std::string& url);
+
+    static bool download(
+        const std::string& url,
+        const std::string& outputPath
+    );
 };
 
 }
